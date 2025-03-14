59d01cf7579116002d9e30525e65f7e8aa880fec
AristonCatipay <aristoncatipay123@gmail.com>
2025-03-14 21:25:19 +0800

    Finish the discussion for asynchronous javascript.

---
+++ [09] - JavaScript Asynchronous/3-callback-hell.js
@@ -0,0 +1,35 @@
+// Callback:
+// - is a function that is passed as an argument to another function.
+// - callbacks are used to handle asynchronous operations.
+
+// Callback Hell
+
+const taskOne = (callback) => {
+  setTimeout(() => {
+    console.log("Task 1 complete.");
+    callback();
+  }, 3000);
+};
+
+const taskTwo = (callback) => {
+  setTimeout(() => {
+    console.log("Task 2 complete.");
+    callback();
+  }, 2000);
+};
+
+const taskThree = (callback) => {
+  setTimeout(() => {
+    console.log("Task 3 complete.");
+    callback();
+  }, 1000);
+};
+
+// Nested Callback: Callback Hell
+taskOne(() => {
+  taskTwo(() => {
+    taskThree(() => {
+      console.log("All tasks complete.");
+    });
+  });
+});
