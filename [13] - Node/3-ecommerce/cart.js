1c081301ca75ffa3f5a7603c943d6237ddac662c
AristonCatipay <aristoncatipay123@gmail.com>
2025-03-27 22:09:14 +0800

    Finish discussion for global object, modules, and ecommerce.

---
+++ [13] - Node/3-ecommerce/cart.js
@@ -0,0 +1,24 @@
+let cart = [];
+
+function displayCart() {
+  console.log("Cart:", cart);
+}
+
+function addToCart(productObject) {
+  console.log(`${productObject.name} added to cart.`);
+  cart.push(productObject);
+}
+
+function calculateTotal() {
+  let total = 0;
+  cart.forEach((productObject) => {
+    total += productObject.price;
+  });
+  return total;
+}
+
+module.exports = {
+  displayCart,
+  addToCart,
+  calculateTotal,
+};
